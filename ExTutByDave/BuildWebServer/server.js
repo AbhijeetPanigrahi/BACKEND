3184290bfa2ba1391285b3b4b5c0dcdec993b849
AbhijeetPanigrahi <abhijeetpanigrahi912@gmail.com>
2024-03-15 00:20:14 +0530

    Middleware in ExpressJS

--- ExTutByDave/BuildWebServer/server.js
+++ ExTutByDave/BuildWebServer/server.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+// creating a port
+const PORT = process.env.PORT || 3500;
 
-app.get("/index(.html)?", (req, res) => {
+// custom middleware logger
+
+/*
+app.use((req, res, next) => {
+  logEvents(
+    `${req.method} \t ${req.headers.origin} \t ${req.url}`,
+    "reqLog.txt"
+  );
+  console.log(`${req.method} , ${req.path}`);
+  next();
+});           
+*/
+app.use(logger);
+
+//  Cross Origin Resource Shairing    // (for cors error in google console)
+app.use(cors());
+
+// built-in middleware to handle urlencoded data
+// in other words form data
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for JSON
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("^/$|/index(.html)?", (req, res) => {
   // res.send("hello world");
   // res.sendFile("./view/index.html", { root: __dirname });    (we do this in node)
   res.sendFile(path.join(__dirname, "view", "index.html"));
@@ -48,8 +80,5 @@ app.get("/chain(.html)?", [one, two, three]);
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "view", "404.html"));
 });
-
-// creating a port
-const PORT = process.env.PORT || 3500;
 
 app.listen(PORT, () => console.log(`Server is working on port ${PORT}`));
