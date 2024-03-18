6aebe93f9e4285ae02f04444a2cbc4f6d6a2cb18
AbhijeetPanigrahi <abhijeetpanigrahi912@gmail.com>
2024-03-18 21:21:55 +0530

    #reStart

---
+++ Chai_backend/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/linkedin", (req, res) => {
+  res.send("let's grow in linkedin");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Log in here</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
